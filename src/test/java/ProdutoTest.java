965e45abedb483a790734c21efd0abe52ea75372
Bruno Arial <brunoarial@gmail.com>
2026-09-21 16:37:42 -0300

    test: Adicionar testes de estoque do produto

--- src/test/java/ProdutoTest.java
+++ src/test/java/ProdutoTest.java
@@ -1,6 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ProdutoTest {
 
@@ -17,5 +18,37 @@ public class ProdutoTest {
         produto.reduzirEstoque(3);
 
         assertEquals(7, produto.getEstoque());
+    }
+
+    @Test
+    void deveImpedirReducaoMaiorQueEstoque() {
+
+        Produto produto = new Produto(
+            1,
+            "Produto Teste",
+            new BigDecimal("10.00"),
+            10
+        );
+
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> produto.reduzirEstoque(20)
+        );
+    }
+
+    @Test
+    void deveImpedirQuantidadeZero() {
+
+        Produto produto = new Produto(
+            1,
+            "Produto Teste",
+            new BigDecimal("10.00"),
+            10
+        );
+
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> produto.reduzirEstoque(0)
+        );
     }
 }
